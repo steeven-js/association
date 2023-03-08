dd930c23644fceed623a5a04d747c10be2b305ce
steeven-js <jacques.steeven@gmail.com>
2023-03-08 18:59:47 -0400

    inscrption connexion

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
+    {
+        $notification = null;
+
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+
+            if (!$search_email) {
+                $password = $encoder->hashPassword($user,$user->getPassword());
+
+                $user->setPassword($password);
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+            } else {
+                $notification = "L'email que vous avez renseigné existe déjà.";
+            }
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
